45a1856705c46e23eb42ee6ce4d143d1c52337a0
Nathaniel Aquino <37340195+Naquino14@users.noreply.github.com>
2022-02-08 23:33:17 -0500

    Write backbone and a few funcs
    https://www.desmos.com/calculator/smzvw3bem6
    
    yea im crazy asf

--- AKG-1/AKG-1/AKG1.cs
+++ AKG-1/AKG-1/AKG1.cs
@@ -1,10 +1,202 @@
 ﻿using System;
 using c = System.Console;
+using System.Buffers.Binary;
 
 namespace ADIS
 {
     public class AKG1
     {
+        /// ppl reading this must think im crazy or someth
+        /// so, heres a disclaimer:
+        /// Im not some top teir mathemetician
+        /// im just some crazy highschooler with crazy ideas that arent backed by solid proof
+        /// anyways, on with the brain vomit
+        /// Rules:
+        /// AKG Must not be used for encrypting data
+        /// Generated keys must be 16 bytes (128 bits) in length
+        /// The algorithm must generate exactly two keys that are ONLY related mathematically
+        /// Foreword: This is probably going to be the most difficult algorithm to create and implement. 
+        /// The fact that it took 3 MIT students and an entire year to do it is a bit scary.
+        /// Even if its a little less secure, I want to try my best to NOT base this off of anything, at least for the time being.
+        /// 
+        /// Food for thought
+        /// Finding keys from a specified input (usually random, and large) should be easy
+        /// Finding the private key from the public key should be computationally difficult
+        /// Finding the specified input from the public key should be computationally difficult
+        /// 
+        /// From what I understood from grazing over RSA is that it uses a bunch of, in terms
+        /// of reversibility, computationally difficult mathematics. 
+        /// 
+        /// The enceyption and deceyption part of AKG-1 only happens once. 
+        /// Speed is not a topic of worry for me.
+        /// 
+        /// Ideadump:
+        /// > find a large input salt and mix it somehow with a difficult to reverse function
+        /// > Look around, ideas can come from anywhere
+        /// > calculus?
+        /// 
+        /// The basic idea is as such:
+        /// If i perform a mathematical operation with the first key in a pair on data d, the same or similar mathematical
+        /// operation with the 2nd key pair on data d gets the data d decrypted. 
+        /// In RSA this mathematical operation is an exponent and its inverse.
+        /// 
+        /// prvk := private key
+        /// pubk := public key
+        /// cssk := ciphered shared secret key (gets thrown into arc)
+        /// pssk =: plaintext shared secret key
+        /// s := salt
+        /// > p * pubk = c => encryption
+        /// > c * prvk = p => decryption
+        /// the problem here is that cssk / pubk = pssk
+        /// 
+        /// Other potential solutions:
+        /// A key generation algorithm based on https://en.wikipedia.org/wiki/Diffie%E2%80%93Hellman_key_exchange
+        /// Ok I said I wouldnt base this off of anything but this is much simpler than RSA.
+        /// But this idea of 4 keys in total instead of 3 is a lot nicer. 
+        /// https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Diffie-Hellman_Key_Exchange.svg/375px-Diffie-Hellman_Key_Exchange.svg.png
+        /// This image illustrates the genius idea
+        /// 
+        /// Demo 1: Public key structure: 
+        ///  0: mod byte 1
+        ///  1: mod byte 2
+        ///  2: mod byte 3
+        ///  3: mod byte 4
+        ///  4: exponent byte 1 | floating point
+        ///  5: exponent byte 2
+        ///  6: exponent byte 3
+        ///  7: exponent byet 4
+        ///  8: salty byte
+        ///  9: coef 1 byte 1 | poly 1
+        /// 10: coef 1 byte 2
+        /// 11: coef 1 byte 3
+        /// 12: coef 1 byte 4
+        /// 13: coef 2 byte 1
+        /// 14: coef 2 byte 2
+        /// 15: coef 2 byte 3
+        /// 16: coef 2 byte 4
+        /// 17: coef 3 byte 1
+        /// 18: coef 3 byte 2
+        /// 19: coef 3 byte 3
+        /// 20: coef 3 byte 4
+        /// 21: coef 4 byte 1
+        /// 22: coef 4 byte 2
+        /// 23: coef 4 byte 3
+        /// 24: coef 4 byte 4
+        /// 25: coef 1 byte 1 | poly 2
+        /// 26: coef 1 byte 2
+        /// 27: coef 1 byte 3
+        /// 28: coef 1 byte 4
+        /// 29: coef 2 byte 1
+        /// 30: coef 2 byte 2
+        /// 31: coef 2 byte 3
+        /// 32: coef 2 byte 4
+        /// 33: coef 3 byte 1
+        /// 34: coef 3 byte 2
+        /// 35: coef 3 byte 3
+        /// 36: coef 3 byte 4
+        /// 37: coef 4 byte 1
+        /// 38: coef 4 byte 2
+        /// 39: coef 4 byte 3
+        /// 40: coef 4 byte 4
+        /// ok coming up with a base idea took alot less time than I thought LMFAO
+        /// https://www.desmos.com/calculator/xnv1ng9lz4
+        /// I can already tell its gonna be slow asf but i just want to get a working model for now
+        /// as a side note im prob also not gonna bother with regions
 
+        public struct PublicKey
+        {
+            public int mod; 
+            public float exponent; 
+            public byte salt; 
+            public int[] poly1Coefs; 
+            public int[] poly2Coefs;
+        }
+
+        public byte[] MixSecrets()
+        {
+            // this is Pf(x) on desmos
+            throw new NotImplementedException();
+        }
+
+        private byte[] GetSharedSecret(byte[] pmKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int SolvePoly(in int[] cs, int x)
+        {
+            // this can probably be made to be much faster
+            int result = 0;
+            for (int i = 3, j = 0; i > 0; i--, j++)
+                result += cs[j] * EBSMK(x, i, 0xFF);
+            result += cs[3];
+            return result;
+        }
+
+        public static PublicKey DeMarshalPublicKey(byte[] incoming)
+        {
+            var key = new PublicKey();
+            key.mod = B2I(incoming[0], incoming[1], incoming[2], incoming[3]);
+            key.exponent = B2F(in incoming);
+            key.salt = incoming[8];
+            key.poly1Coefs = new int[] // its 11 pm i cant be bothered to toss this in a loop rn
+            {
+                B2I(incoming[9], incoming[10], incoming[11], incoming[12]),
+                B2I(incoming[13], incoming[14], incoming[15], incoming[16]),
+                B2I(incoming[17], incoming[18], incoming[19], incoming[20]),
+                B2I(incoming[21], incoming[22], incoming[23], incoming[24])
+            };
+            key.poly2Coefs = new int[]
+            {
+                B2I(incoming[25], incoming[26], incoming[27], incoming[28]),
+                B2I(incoming[29], incoming[30], incoming[31], incoming[32]),
+                B2I(incoming[33], incoming[34], incoming[35], incoming[36]),
+                B2I(incoming[37], incoming[38], incoming[39], incoming[40])
+            };
+            return key;
+        }
+
+        public static byte[] MarshalPublicKey(PublicKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static PublicKey GeneratePublicKey(int seed = 1)
+        {
+            seed = (seed == 0) ? seed++ : seed;
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Exponentation By Squaring Mod K
+        /// </summary>
+        public static int EBSMK(int x, int n, int k)
+        {
+            int res = 1;
+            x %= k;
+            if (x == 0)
+                return 0;
+            while (n > 0)
+            {
+                if ((n & 1) != 0)
+                    res = res * x % k;
+                n >>= 1;
+                x = x * x % k;
+            }
+            return res;
+        }
+
+        public static int B2I(byte a, byte b, byte c, byte d)
+        { return BinaryPrimitives.ReadInt32LittleEndian(new byte[] { a, b, c, d }); }
+
+        public static float B2F(in byte[] a)
+        {
+            return (a.Length < 4)
+                ? throw new ArgumentException()
+                : BitConverter.ToSingle(a, 4);
+        }
+
+        public static byte[] I2B(int a)
+        { return BitConverter.GetBytes(a); }
     }
 }
--- AKG-1/AKG-1/Program.cs
+++ AKG-1/AKG-1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using c = System.Console;
+using ADIS;
 
 namespace Sandbox
 {
@@ -8,6 +9,32 @@ namespace Sandbox
         public static void Main(string[] args)
         {
             // uh
+
+            var akg = new AKG1();
+
+            //var result = akg.GenerateKeysT3();
+
+            //c.Write("Generated key 1: ");
+            //foreach (var b in result.Key1)
+            //    c.Write($"0x{b:X}");
+            //c.Write("\nGenerated key 2: ");
+            //foreach (var b in result.Key2)
+            //    c.Write($"0x{b:X}");
+
+            int a = 2, b = 12;
+            int k = 10;
+            c.WriteLine($"{a} ^ {b} % {k} is {AKG1.EBSMK(a, b, k)}");
+
+            int ai = 1245682;
+            byte[] ab = new byte[4];
+            var ar = AKG1.I2B(ai);
+            var ao = AKG1.B2I(ar[0], ar[1], ar[2], ar[3]);
+            c.WriteLine($"{ao} as a byte[] is {{ 0x{ar[0]:X}, 0x{ar[1]:X}, 0x{ar[2]:X}, 0x{ar[3]:X} }}");
+
+            int[] polyC = new int[] { 2, 6, -3, 2 };
+            int s = 3;
+            c.WriteLine($"F(x) := {polyC[0]}x^3 + {polyC[1]}x^2 + {polyC[2]}x + {polyC[3]} | F({s}) = {AKG1.SolvePoly(in polyC, s)}");
+
         }
     }
 }
